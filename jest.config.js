36332c61028d44e9fd4ce4da7dcd758fc059b5da
Eduardo Reveles <me@osiux.ws>
2020-06-15 05:36:04 -0500

    Fixing tests

--- jest.config.js
+++ jest.config.js
@@ -8,10 +8,10 @@ module.exports = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     snapshotSerializers: ['jest-emotion'],
     moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>/__mocks__/fileMock.js',
         '^@client(.*)$': '<rootDir>/src/client/$1',
         '^@server(.*)$': '<rootDir>/src/server/$1',
     },
-    transformIgnorePatterns: [
-        'node_modules/(?!(ky)/)',
-    ],
+    transformIgnorePatterns: ['node_modules/(?!(ky)/)'],
 };
